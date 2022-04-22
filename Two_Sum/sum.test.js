ffcb1339b80631d5631e4dd288425fcf258d9662
Shantanu Jana <omni.7x@gmail.com>
2022-04-22 11:24:10 +0530

    update sum.test.js

--- Two_Sum/sum.test.js
+++ Two_Sum/sum.test.js
@@ -6,3 +6,7 @@ test('add 1 + 2 to equal 3', () => {
 test('add 10 + 90 to equal 100', () => {
     expect(sum(10,90)).toBe(100);
 });
+
+test('add 255 + 135 to equal 390', () => {
+    expect(sum(255,135)).toBe(390);
+});
